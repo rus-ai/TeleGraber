2246f210f42dbf7e3bc06c273a7951561090bed4
rus-ai <russian-ai@yandex.ru>
2020-11-27 19:21:24 +0300

    First ready

--- app_telegrabber.py
+++ app_telegrabber.py
@@ -1,22 +1,33 @@
 from telethon import TelegramClient, events
 import config
-import asyncio
 
 client = TelegramClient(config.app_name, config.api_id, config.api_hash)
 
+
 print("Work begin")
+
 
 @client.on(events.NewMessage(chats=config.channels))
 async def my_event_handler(event):
+    print(event.message)
+    for word in config.stop_words:
+        if word in event.message.text:
+            print("Spam post skipped")
+            return
     if event.message.text:
-        print("Message event")
+        print("Text event")
         await client.send_message(config.my_channel_id, event.message)
+        return
     if event.message.media:
         print("Media event")
         await client.send_file(config.my_channel_id, file=event.message)
+        return
+    print("Unhandled message type")
+
 
 @client.on(events.Album(chats=config.channels))
 async def handler(event):
+    print("Album event")
     await client.send_message(
         config.my_channel_id,
         file=event.messages,
